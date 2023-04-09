286be7ca027a15b914e46b37ee9e5b398f89367b
guzbekova <guzbekova@ozon.ru>
2023-04-09 12:25:09 +0400

    Added ReqresInTests

---
+++ src/test/java/ReqresInTests.java
@@ -0,0 +1,107 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresInTests {
+
+    @Test
+    @DisplayName("Checking user first name in Single User")
+    void FirstNameInSingleUserTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.first_name", is("Janet"));
+    }
+
+
+    @Test
+    @DisplayName("Checking user email in User List")
+    void EmailInListUserTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data[3].email", is("byron.fields@reqres.in"));
+    }
+
+    @Test
+    @DisplayName("Checking single user: user not found")
+    void SingleUserNotFoundTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("Checking login unsuccessful")
+    void LoginErrorTest() {
+        String data = "{ \"email\": \"testEmail@test\", \"password\": \"test\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("user not found"));
+    }
+
+    @Test
+    @DisplayName("Checking login successful")
+    void LoginSuccessfulTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    @DisplayName("Checking update user")
+    void UpdateUserTest() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+}
+
